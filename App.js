0bc8216f73c58163dcc497c057404492c4b0d03c
Paul Diwa <poldiwa@gmail.com>
2022-09-10 04:44:54 +0800

    sample mtx react native web view

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const webViewRef = useRef(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      onMessage={(event) => {
+        let postMessage = JSON.parse(event.nativeEvent.data)
+
+        // looks like yodlee is directly sending it to the ReactNativeWebview 
+        // can be something we can tell to yodlee but we can just relay it for now
+
+        // @see: https://developer.yodlee.com/docs/fastlink/4.0/mobile/ReactNative
+        if (postMessage.type == "POST_MESSAGE") {
+          webViewRef.current.injectJavaScript(`window.postMessage(${JSON.stringify(postMessage.data)});`);
+        }
+
+        // we'll do something what yodlee do and check for ReactNativeWebview and postmessage with it
+        // so you dont have to convert the events, 
+        // events are 
+        // EXIT
+        // ENROLLMENT_SUCCESS
+        // ENROLLMENT_FAILED
+        // ERROR
+        // SESSION_EXPIRED
+        // PLAYER_RETRIEVED
+        if (postMessage.namespace == "mtx" && postMessage.event == "PLAYER_RETRIEVED") {
+          // do something
+          console.log('log from react native')
+          // alert('alert from react native')
+        }
+      }}
+      ref={webViewRef}
+      source={{ uri: 'https://markertrax-player-apps.stn-uat.markertrax.com/7/1001/dashboard/?linkedId=ABFXBVFD2NBQKJYDUKHHMPBBK6SNHR' }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
